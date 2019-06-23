3534bad470a54923ff9ae7311ea733e2998eb43e
Artemiy <rendrom@gmail.com>
2019-06-23 22:02:24 +0800

    Add english description

--- src/components/Links/Links.ts
+++ src/components/Links/Links.ts
@@ -10,6 +10,8 @@ import pkg from '../../../package.json';
 import './img/nextgis.png';
 import { App } from 'src/App';
 import { SliderOptions } from '../SliderControl';
+import { aboutShortRu } from './aboutRu';
+import { aboutShortEn } from './aboutEn';
 
 export function getSwitcherPanelControl(controls: Controls) {
   const block = document.createElement('div');
@@ -55,7 +57,8 @@ export function getSocialLinksPanel() {
 
 export function getAboutProjectLink(app: App) {
   const block = document.createElement('a');
-  let template = aboutShort;
+  // let template = aboutShortRu;
+  let template = aboutShortEn;
   block.className = 'about_icon';
   block.setAttribute('href', '#');
   block.innerHTML = `i`;
@@ -295,176 +298,6 @@ function getAboutBlock(block: string) {
     </P>`;
 }
 
-const aboutShort = `
-<div style="margin-top: 40px;"></div>
-<div class="partner_logos">
-<a href="https://www.runivers.ru" target="_blank"><img src="images/Runivers-Logo-color.svg" /></a>
-<a href="https://www.transneft.ru" target="_blank"><img src="images/Transneft_logo1.png" /></a>
-<a href="https://histgeo.ru/laboratory.html" target="_blank"><img src="images/geolab.png" /></a>
-<a href="https://nextgis.ru" target="_blank"><img src="images/nextgis.png" /></a>
-</div>
-<div style="margin-top: 40px;"></div>
-<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0.17in">
-<h2>О проекте Границы России 1462-2018 гг.</h2></P>
-<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0.17in">
-<SPAN LANG="ru-RU"><a href="https://www.runivers.ru" target="_blank">«Руниверс»</a>
-совместно с <a href="https://histgeo.ru/laboratory.html" target="_blank">Лабораторией исторической
-геоинформатики</a> Института всеобщей
-истории РАН и компанией <a href="http://nextgis.ru/" target="_blank">NextGIS</a>
-представляет проект «Границы России
-1462–2018 гг.».</SPAN></P>
-<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0.17in">
-<SPAN LANG="ru-RU">Этот
-картографический проект содержит
-геоданные о политических границах
-современной России и ее предшественников.
-Важнейшей возможностью, которую
-предоставляет данная карта, является
-выбор любого года в интервале карты.
-Год можно выбрать, кликнув мышкой в
-нужном месте ленты времени (в нижней
-части карты), либо запустив автоматическую
-прокрутку ленты времени с шагом в один
-год, либо, используя иконки + и – справа
-от ленты времени, сдвигаться на один
-год влево или вправо.</SPAN></P>
-<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0.17in">
-<A NAME="OLE_LINK210"></A><A NAME="OLE_LINK209"></A>
-<SPAN LANG="ru-RU">Понимая,
-с одной стороны, что формы политического
-статуса территорий многообразны и, с
-другой стороны, что это многообразие
-необходимо свести к ограниченному числу
-вариантов при отображении на карте, мы
-выделили всего семь различных статусов
-территории России или ее предшественников:</SPAN></P>
-<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0in">
-<SPAN LANG="ru-RU">1) Основная
-территория государства.</SPAN></P>
-<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0in">
-<SPAN LANG="ru-RU">2) Территория
-под протекторатом, в вассальной
-зависимости или в сфере влияния.</SPAN></P>
-<P LANG="ru-RU" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0in">
-3) Арендованная
-территория.</P>
-<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0in">
-<SPAN LANG="ru-RU">4) Территория
-в совместном владении.</SPAN></P>
-<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0in">
-<SPAN LANG="ru-RU">5) Спорная
-территория.</SPAN></P>
-<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0.17in">
-<SPAN LANG="ru-RU">Кроме
-того, в год изменения конфигурации
-границы соответствующий участок показан
-отдельным полигоном: как 6) Новая
-территория или, напротив, как 7) Утраченная
-территория. Эти полигоны также снабжены
-маркерами, цифра на которых обозначает
-соответствующую позицию в списке
-территориальных изменений информационной
-панели. В связи с тем, что некоторые
-изменения территории имели место в
-разные месяцы года, иногда полигоны со
-статусами 6 и 7 задерживаются на карте
-и в следующем после события году.</SPAN></P>
-<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0.17in">
-<A NAME="OLE_LINK222"></A><A NAME="OLE_LINK221"></A>
-<SPAN LANG="ru-RU">Эта
-дифференциация отражена во всплывающем
-окне, вызываемом по клику мыши на любом
-объекте внутри политических границ.
-Заливка соответствующих полигонов
-передана четырьмя цветами: одним цветом
-показана основная территория государства
-и вновь присоединенные земли (статусы
-1 и 6), другим цветом – земли, находящиеся
-в некоторой зависимости от него (статус
-2), третьим – земли арендованные, спорные
-и находящиеся под совместным управлением
-с другими государствами (статусы 3–5).
-Утраченные территории обозначены
-четвертым цветом (статус 7), однако в
-силу ряда технических особенностей
-текущего алгоритма построения геометрий
-не все утраченные территории показаны
-на карте отдельным полигоном. По клику
-на любой полигон можно получить данные
-о хронологическом диапазоне, в котором
-существовало данное территориальное
-образование, и его площади (в квадратных
-километрах).</SPAN></P>
-<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0.17in">
-<SPAN LANG="ru-RU">В центре
-каждого такого полигона помещен маркер,
-помогающий пользователю быстро найти
-соответствующее место на мелкомасштабной
-карте. По клику на маркер выдается
-информация о событии, приведшем к
-появлению данного территориального
-образования.</SPAN></P>
-<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0.17in">
-<SPAN LANG="ru-RU">В правом
-верхнем углу карты доступны три
-иконки-кнопки. Первая позволяет отключить
-современную картографическую подложку,
-две другие позволяют управлять
-отображением двух информационных блоков
-карты. В первом из них отображается имя
-правителя на избранный пользователем
-год, его титул или должность, годы
-нахождения у власти с отсылкой на
-отдельную страницу сайта с более
-подробными сведениями. В нижнем блоке
-указаны имевшие место в данном году
-территориальные изменения и приведена
-приблизительная площадь участков,
-которые добавились к территории
-государства и, наоборот, оказались
-утрачены.</SPAN></P>
-<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0.17in">
-<SPAN LANG="ru-RU">При
-создании веб-ГИС «Границы Московского
-княжества/Русского царства/Российской
-империи/СССР/Российской федерации
-(1462–2018 гг.)» реализована концепция
-динамической карты. С точки зрения
-технической реализации проекта эта
-концепция является оригинальным
-решением, разработанным в <a href="https://histgeo.ru/laboratory.html" target="_blank">Лабораторией исторической
-геоинформатики</a> Института
-всеобщей истории РАН совместно с
-<a href="http://nextgis.ru/" target="_blank">NextGIS</a>.
-Описание <a href="http://nextgis.ru/blog/runivers/" target="_blank">технической реализации</a> проекта.
-</SPAN></P>
-<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0.17in">
-<SPAN LANG="ru-RU">Прикладное
-значение данного проекта для использования
-в научных целях является отличительной
-его чертой. Подобные динамические карты,
-демонстрирующие изменения границ в
-прошлом, имеют в основном «образовательный»
-уклон и не могут быть скорректированы
-в результате обсуждения кругом
-специалистов-историков. Публикуемая
-динамическая карта, открытая для
-постоянной корректировки, призвана в
-дальнейшем стать некой картографической
-подосновой для историка, применяющего
-ГИС-технологии для решения конкретных
-исследовательских задач.</SPAN></P>
-<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0.17in">
-<SPAN LANG="ru-RU">О допущениях,
-особенностях методики и ограничениях
-применявшейся методики, технических
-особенностях проекта и перспективах
-его развития см. <a
-  href="https://www.runivers.ru/granitsy-rossii/about/about_detailed/index.php"
-  target="_blank"
->
-  ПОДРОБНЕЕ
-</a>.</SPAN></P>
-`;
+
 
 
---
+++ src/components/Links/aboutEn.ts
@@ -0,0 +1,129 @@
+export const aboutShortEn = `
+<div style="margin-top: 40px;"></div>
+<div class="partner_logos">
+<a href="https://www.runivers.ru" target="_blank"><img src="images/Runivers-Logo-color.svg" /></a>
+<a href="https://www.transneft.ru" target="_blank"><img src="images/Transneft_logo1.png" /></a>
+<a href="https://histgeo.ru/laboratory.html" target="_blank"><img src="images/geolab.png" /></a>
+<a href="https://nextgis.ru" target="_blank"><img src="images/nextgis.png" /></a>
+</div>
+<div style="margin-top: 40px;"></div>
+<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0.17in">
+<h2>Russia’s Borders from 1462 through 2018’ Project</h2></P>
+<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0.17in">
+
+<SPAN LANG="en-EN"><a href="https://www.runivers.ru" target="_blank">Runivers</a>
+
+presents ‘Russia’s Borders from 1462 through 2018’ –
+a project designed in collaboration with the
+<a href="https://histgeo.ru/laboratory.html" target="_blank">
+Laboratory of Historical Geomatics</a> at the Institute of World History
+of the Russian Academy of Sciences (RAS) and
+<a href="http://nextgis.ru/" target="_blank">NextGIS Ltd.</a>
+</SPAN></P>
+
+<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0.17in">
+<SPAN LANG="en-EN">
+This cartographic project contains geographical information on the political borders
+of the contemporary Russian Federation and its predecessor states.
+An opportunity to select any year in the 1462 to 2018 time bracket is a crucial option of this map.
+To choose the needed year, the users either click at the needed point on the timeline (at the bottom of the map)
+or launch its step-by-step automatic rewinding or else use the + or –
+icons to the right of the timeline, thus moving one year backwards or forwards.
+</SPAN></P>
+
+<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0.17in">
+<A NAME="OLE_LINK210"></A><A NAME="OLE_LINK209"></A>
+<SPAN LANG="en-EN">
+The realization that the forms of a political status of lands are multiple and that,
+on the other hand, this diversity should be reduced to a limited number of versions for
+depiction on the map has prompted us to single out seven different statuses of the territory
+of today’s Russia and its predecessors.
+</SPAN></P>
+<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0in">
+<SPAN LANG="en-EN">
+1) The mainland of the state.
+</SPAN></P>
+<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0in">
+<SPAN LANG="en-EN">
+2) ТA territory under protectorate, vassal allegiance or in the sphere of influence.
+</SPAN></P>
+<P LANG="en-EN" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0in">
+3) АA leased territory.
+</P>
+<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0in">
+<SPAN LANG="en-EN">
+4) A territory in joint possession.
+</SPAN></P>
+<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0in">
+<SPAN LANG="en-EN">
+5) A litigious territory.
+</SPAN></P>
+<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0.17in">
+
+<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0.17in">
+<A NAME="OLE_LINK222"></A><A NAME="OLE_LINK221"></A>
+<SPAN LANG="en-EN">
+Changes in the configuration of the border in relevant years are shown in separate polygons as: 6.
+the newly acquired territories or, 7. lost territories.
+These polygons have the tokens, the numbers on which correspond to positions in the list of
+territorial changes found in the info bar.
+Territorial changes occurred in different months of the year – sometimes in the first half of the year,
+and sometime in the second half, and that is why the polygons with statuses 6 and 7 sometimes stay on
+and get over to the year that followed a specified event.
+</SPAN></P>
+<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0.17in">
+<SPAN LANG="en-EN">
+The differentiation is reflected in popup windows,
+which the users can bring up by clicking at any object inside the political borders.
+The polygons are filled with four colors. One color shows the mainland of the state and the newly
+incorporated lands (statuses 1 and 6); the second color marks the lands that have a certain degree
+of dependence on the state (status 2); the third color brings out the leased and litigious areas or
+the lands governed jointly with other countries (statuses 3 and 5).
+The lost territories (status 7) are marked with the fourth color but owing to certain technology
+peculiarities of the current algorithm of geometrical arrangements formation some of the lost territories
+are not displayed on the map with the aid of separate polygons. A click at any polygon will call up
+the information on the chronological range, in which a territorial entity existed, and on its size (in sq. km).
+</SPAN></P>
+
+<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0.17in">
+<SPAN LANG="en-EN">
+Placed in the center of each polygon is a token that helps the users find an appropriate
+location on a small-scale map. A click at the token will retrieve the data on the event
+that resulted in the formation of the specific territorial entity.
+</SPAN></P>
+<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0.17in">
+<SPAN LANG="en-EN">
+Three button icons are available in the upper right-hand corner of the map.
+The first one switches the contemporary basemap off, while two others make it
+possible to handle the presentation of two data units of the map.
+The first unit shows the name, title/ official position and the years in office of the person
+who ruled the territory in the year chosen by the user. It also contains a reference to a special page
+at the website that offers detailed information. The lower data unit provides information
+on the territorial changes in the selected year, as well as on an approximate size of the lands
+that were incorporated into the nation or, vice versa, were lost.
+</SPAN></P>
+<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0.17in">
+<SPAN LANG="en-EN">
+‘The Borders of the Duchy of Muscovy/ Czardom of Russia/ Russian Empire/ USSR/
+Russian Federation from 1462 through 2018’ web GIS rests on the concept of a dynamic map.
+As regards the technical implementation, this concept embodies an original solution developed at the
+<a href="https://histgeo.ru/laboratory.html" target="_blank">Laboratory of Historical Geomatics</a>
+of the RAS Institute of World History in cooperation with
+<a href="http://nextgis.ru/" target="_blank">NextGIS Ltd.</a>
+</SPAN></P>
+
+<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0.17in">
+<SPAN LANG="en-EN">
+Practical applicability of this project for research purposes makes it stand out among other projects of this kind.
+The dynamic maps displaying the past changes of state borders most typically have an educational
+focus and historians do not have a chance to amend them after expert debates.
+Conversely, the dynamic map released now is open for future amendments and is called upon
+to serve as an instrumental cartographic aid for the scholars of history who use GIS technologies
+to attain specified research objectives.
+
+
+<P LANG="en-GB" CLASS="western" ALIGN=JUSTIFY STYLE="margin-bottom: 0.17in">
+<SPAN LANG="en-EN">
+Technical implementation  <a href="http://nextgis.com/blog/runivers/" target="_blank">description</a>.
+</SPAN></P>
+`;
