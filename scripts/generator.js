2f7b95d915681703fa7e26f182a4ee36ee7b278d
Artemiy <rendrom@gmail.com>
2020-02-25 17:03:55 +0800

    feat: change generator script

--- scripts/generator.js
+++ scripts/generator.js
@@ -3,46 +3,57 @@
  */
 const fs = require('fs');
 const NgwConnector = require('@nextgis/ngw-connector');
-const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
-const baseUrl = config.baseUrl;
 
-const out = './src/data/layers.json';
+function generator(options) {
+  const out = options.out || './src/data/layers.json';
+  const configPath = options.config || 'config.json';
+  const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
+  const baseUrl = config.baseUrl;
+  const connector = new NgwConnector({ baseUrl });
 
-const connector = new NgwConnector({ baseUrl });
-
-function generateData(data) {
-  const toFile = [];
-  data.forEach(x => {
-    toFile.push({
-      name: x.name,
-      items: x.items.map(x => {
-        const res = x.resource;
-        return { resource: { display_name: res.display_name, id: res.id } };
-      })
+  function generateData(data) {
+    const toFile = [];
+    data.forEach(x => {
+      toFile.push({
+        name: x.name,
+        items: x.items.map(x => {
+          const res = x.resource;
+          return { resource: { display_name: res.display_name, id: res.id } };
+        })
+      });
     });
+
+    fs.writeFile(out, JSON.stringify(toFile), () =>
+      console.log('Data write in `' + out + '` file')
+    );
+  }
+
+  const promises = config.sourceGroups.map(x => {
+    console.log(`Start loading layer ${x.name} resource from ${x.resourceId}`);
+    return connector
+      .getResourceChildren({ resourceId: x.resourceId })
+      .then(items => {
+        return { ...x, items };
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
   });
 
-  fs.writeFile(out, JSON.stringify(toFile), () =>
-    console.log('Data write in `' + out + '` file')
-  );
-}
-
-const promises = config.sourceGroups.map(x => {
-  console.log(`Start loading layer ${x.name} resource from ${x.resourceId}`);
-  return connector
-    .getResourceChildren({ resourceId: x.resourceId })
-    .then(items => {
-      return { ...x, items };
+  Promise.all(promises)
+    .then(data => {
+      generateData(data);
     })
     .catch(err => {
-      throw new Error(err);
+      console.log(err);
     });
-});
+}
 
-Promise.all(promises)
-  .then(data => {
-    generateData(data);
-  })
-  .catch(err => {
-    console.log(err);
+if (require.main === module) {
+  const argv = require('minimist')(process.argv.slice(2));
+  generator({
+    out: argv.o,
+    config: argv.c
   });
+}
+module.exports = generator;
