39aefe655f5e6d2ee453e726b12078dc795cce3e
sirflyingv <andthenyouadd@gmail.com>
2023-07-05 15:55:50 +0300

    types: add missing file types

--- custom_typings/loaders.d.ts
+++ custom_typings/loaders.d.ts
@@ -1,9 +1,26 @@
-declare module "*.json" {
-    const value: any;
-    export default value;
+declare module '*.json' {
+  const value: any;
+  export default value;
 }
 
-declare module "*.csv" {
+declare module '*.csv' {
+  const value: any;
+  export default value;
+}
+
+// lower added to work without submodule
+
+declare module '*.png' {
+  const value: any;
+  export default value;
+}
+
+declare module '*.jpg' {
+  const value: any;
+  export default value;
+}
+
+declare module '*.svg' {
   const value: any;
   export default value;
 }
