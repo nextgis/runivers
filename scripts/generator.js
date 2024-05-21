4ca78af1e19fb260e566cee214a1e14c9167c168
Artemiy <rendrom@gmail.com>
2024-05-21 15:56:31 +0200

    fix(scripts): correct request that breaks NGW

--- scripts/generator.js
+++ scripts/generator.js
@@ -1,59 +1,71 @@
 /**
- * This node script allow to generate actual data for layer from http://213.248.47.89
+ * This node script allows generating actual data for layers from http://213.248.47.89
  */
 const fs = require('fs');
+
 const NgwConnector = require('@nextgis/ngw-connector');
 
-function generator(options) {
+async function generator(options) {
   const out = options.out || './src/data/layers.json';
   const configPath = options.config || 'config.json';
   const config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
   const baseUrl = config.baseUrl;
   const connector = new NgwConnector({ baseUrl });
 
-  function generateData(data) {
-    const toFile = [];
-    data.forEach(x => {
-      toFile.push({
-        name: x.name,
-        items: x.items.map(x => {
-          const res = x.resource;
-          return { resource: { display_name: res.display_name, id: res.id } };
-        })
-      });
-    });
+  async function loadLayerData() {
+    const results = [];
 
-    fs.writeFile(out, JSON.stringify(toFile), () =>
-      console.log('Data write in `' + out + '` file')
-    );
+    for (const x of config.sourceGroups) {
+      try {
+        console.log(
+          `Start loading layer ${x.name} resource from ${x.resourceId}`,
+        );
+        const items = await connector.getResourceChildren(x.resourceId, {
+          cache: false,
+        });
+        results.push({ ...x, items });
+      } catch (err) {
+        console.error(`Failed to load data for ${x.name}:`, err);
+      }
+    }
+
+    return results;
   }
 
-  const promises = config.sourceGroups.map(x => {
-    console.log(`Start loading layer ${x.name} resource from ${x.resourceId}`);
-    return connector
-      .getResourceChildren({ resourceId: x.resourceId })
-      .then(items => {
-        return { ...x, items };
-      })
-      .catch(err => {
-        throw new Error(err);
-      });
-  });
+  function generateData(data) {
+    const toFile = data.map((x) => ({
+      name: x.name,
+      items: x.items.map((item) => ({
+        resource: {
+          display_name: item.resource.display_name,
+          id: item.resource.id,
+        },
+      })),
+    }));
 
-  Promise.all(promises)
-    .then(data => {
-      generateData(data);
-    })
-    .catch(err => {
-      console.log(err);
+    fs.writeFile(out, JSON.stringify(toFile), (err) => {
+      if (err) {
+        console.error('Error writing data to file:', err);
+      } else {
+        console.log('Data written to `' + out + '` file');
+      }
     });
+  }
+
+  try {
+    const data = await loadLayerData();
+    generateData(data);
+  } catch (err) {
+    console.error('Error during data processing:', err);
+  }
 }
 
 if (require.main === module) {
   const argv = require('minimist')(process.argv.slice(2));
   generator({
     out: argv.o,
-    config: argv.c
+    config: argv.c,
   });
 }
+
 module.exports = generator;
